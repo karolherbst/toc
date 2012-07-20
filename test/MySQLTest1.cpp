8545550622a7b22685c6a992b0e32e1cff9f4653
Karol Herbst <git@karolherbst.de>
2012-07-20 07:45:35 +0200

    test: add Template for easier DB testing

--- test/MySQLTest1.cpp
+++ test/MySQLTest1.cpp
@@ -1,56 +1,11 @@
 #define BOOST_TEST_MODULE MySQLDriverTest
-#include <boost/test/unit_test.hpp>
 
-#include <toc/tocdb/DB.h>
-#include <toc/tocdb/DBMS.h>
-#include <toc/tocdb/DBResource.h>
+#define PREFERED_DRIVER DBMS::MySQL
+#define QUERY_BUILDER_CLASS MySQLQueryBuilder
+#define ADDITIONAL_AUTH_CALLS \
+db.setUserName("toc_test");   \
+db.setUserPassword("toc_test");
 
 #include "MySQLQueryBuilder.h"
 
-using namespace TOC::DB;
-
-BOOST_AUTO_TEST_CASE( CheckConnection )
-{
-	// set our driver
-	DBResource::Instance().preferedDriver(DBMS::MySQL);
-	DBImpl& db = DB::Instance();
-	db.setDatabaseName("toc_test_case");
-	db.setUserName("toc_test");
-	db.setUserPassword("toc_test");
-	db.initDriver();
-}
-
-BOOST_AUTO_TEST_CASE( QBCreateEntityclass )
-{
-	DBImpl& db = DB::Instance();
-	MySQLQueryBuilder qb;
-
-	try
-	{
-		qb.entityclass("test");
-		db.createTransaction();
-		db.executeQuery(qb.buildDeleteEntityClassQuery(true));
-		db.executeQuery(qb.buildCreateEntityClassQuery());
-
-		// build a table
-		qb.attribute("name");
-		db.executeQuery(qb.buildAddAttributeQuery(String("karol"), TOC::DBString, 32));
-		qb.attribute("age");
-		db.executeQuery(qb.buildAddAttributeQuery(String("0"), TOC::DBInt, 10));
-		qb.attribute("gender");
-		db.executeQuery(qb.buildAddAttributeQuery(String("M"), TOC::DBString, 1));
-
-		// create a row
-		std::map<String, String> values;
-		values["ID"] = "1";
-		values["name"] = "herbst";
-		values["age"] = "21";
-		values["gender"] = "M";
-		// it have to possible to execute this query both;
-		db.executeQuery(qb.buildIdInsertQuery(values));
-		db.executeQuery(qb.buildIdInsertQuery(values));
-
-		db.commit();
-	}
-	catch (DBException &e) {}
-}
+#include "DBTestTemplate.tpp"
--- test/SQLiteTest1.cpp
+++ test/SQLiteTest1.cpp
@@ -1,81 +1,8 @@
 #define BOOST_TEST_MODULE SQLiteTest
-#include <boost/test/unit_test.hpp>
+
+#define PREFERED_DRIVER DBMS::SQLite
+#define QUERY_BUILDER_CLASS SQLiteQueryBuilder
 
 #include "SQLiteQueryBuilder.h"
 
-#include <toc/tocdb/DB.h>
-#include <toc/tocdb/DBMS.h>
-#include <toc/tocdb/DBResource.h>
-#include <toc/tocdb/StandardTypes.h>
-
-using namespace TOC::DB;
-
-BOOST_AUTO_TEST_CASE( CheckConnection )
-{
-    // set our driver
-    DBResource::Instance().preferedDriver(DBMS::SQLite);
-    DBImpl& db = DB::Instance();
-    db.setDatabaseName("toc_test_case");
-	db.initDriver();
-}
-
-BOOST_AUTO_TEST_CASE( QueryBuilder )
-{
-	DBImpl& db = DB::Instance();
-
-	try
-	{
-		SQLiteQueryBuilder qb;
-		qb.entityclass("test");
-		db.createTransaction();
-		db.executeQuery(qb.buildDeleteEntityClassQuery(true));
-		db.executeQuery(qb.buildCreateEntityClassQuery());
-
-		// build a table
-		qb.attribute("name");
-		db.executeQuery(qb.buildAddAttributeQuery(String("karol"), TOC::DBString, 32));
-		qb.attribute("age");
-		db.executeQuery(qb.buildAddAttributeQuery(String("0"), TOC::DBInt, 10));
-		qb.attribute("gender");
-		db.executeQuery(qb.buildAddAttributeQuery(String("M"), TOC::DBString, 1));
-
-		// create a row
-		std::map<String, String> values;
-		values["ID"] = "1";
-		values["name"] = "herbst";
-		values["age"] = "21";
-		values["gender"] = "M";
-		// it have to possible to execute this query both;
-		db.executeQuery(qb.buildIdInsertQuery(values));
-		db.executeQuery(qb.buildIdInsertQuery(values));
-
-		qb.attribute("age");
-		qb.id(1);
-		db.executeQuery(qb.buildSingleValueInsertQuery("22"));
-		db.commit();
-
-		// now we want to check some values stored in the database;
-		std::map<String, String> result;
-		qb.entityclass("test");
-		qb.attribute("");
-		qb.id(1);
-		db.executeSingleRowQuery(qb.buildIDSelectQuery() , result);
-		BOOST_REQUIRE_EQUAL(result.at("ID"), "1");
-		BOOST_REQUIRE_EQUAL(result.at("name"), "herbst");
-		BOOST_REQUIRE_EQUAL(result.at("age"), "22");
-		BOOST_REQUIRE_EQUAL(result.at("gender"), "M");
-
-		// insert a single values
-		db.createTransaction();
-		qb.entityclass("test");
-		qb.attribute("name");
-		qb.id(2);
-		db.executeQuery(qb.buildSingleValueInsertQuery("name1"));
-		db.commit();
-
-		std::map<String, String> result2;
-		db.executeSingleRowQuery(qb.buildIDSelectQuery(), result2);
-		BOOST_REQUIRE_EQUAL(result2.at("name"), "name1");
-	}
-	catch (DBException &e){}
-}
+#include "DBTestTemplate.tpp"
